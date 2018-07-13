4c1fe038d0694e981970ab0d9bb3f5ea80b231aa
kristin <1529908750@qq.com>
2018-07-13 11:22:42 +0800

    v3.0-防作弊版

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 //                          如果不是雷，显示以当前格为中心，周围8个格子里的雷数；如果雷数为0，查找范围显示范围向周围扩散，直至某格周围雷数不为0，停止扩散。
 //                 右键点击：如果已经左键点击过为数字，不操作；如果没有插旗，插上小旗，如果插旗，取消插旗，并计算更新当前剩余雷数；最多只能插旗10个
 // 3. 弹出结束框，点击关闭，关闭结束框。
-// v2.0-限制flagNum
+// 特点：数组存储每个小格位置和对应雷数，结合id查找对应dom；遍历查询每个小格周围雷数；递归扩散周围雷数为0的小格周围的每格周围雷数
+// v3.0-防作弊版，flagNum限制
 var startBtn = document.getElementById('startBtn');
 var flagBox = document.getElementById('flagBox');
 var box = document.getElementById('box');
@@ -15,6 +16,7 @@ var minesNum = 0;
 var mineover = 0;
 var flagNum = 0;
 var minesMap = [];
+var minesArr = [];
 var startGameBool = true;
 
 bindEvent();
@@ -28,7 +30,6 @@ function bindEvent() {
         box.style.display = 'block';
         startGameBool = false;
         init();
-        console.log(minesNum);
     }
     box.oncontextmenu = function () {
         return false;
@@ -60,11 +61,12 @@ function init() {
     score.innerHTML = mineover;
     // 生成棋盘
     for (var i = 0; i < 10; i++) {
+        minesMap.push([]);
         for (var j = 0; j < 10; j++) {
             var block = document.createElement('div');
             block.classList.add('block');
             block.setAttribute('id', 'p' + i + '-' + j);
-            minesMap.push({
+            minesMap[i].push({
                 mine: 0
             });
             box.appendChild(block);
@@ -74,10 +76,14 @@ function init() {
     var block = document.getElementsByClassName('block');
     var num = minesNum;
     while (num) {
-        var mineIndex = Math.floor(Math.random() * 100);
-        if (minesMap[mineIndex].mine == 0) {
-            block[mineIndex].classList.add('isLei');
-            minesMap[mineIndex].mine = 1;
+        var mineX = Math.floor(Math.random() * 10);
+        var mineY = Math.floor(Math.random() * 10);
+        if (minesMap[mineX][mineY].mine == 0) {
+            minesMap[mineX][mineY].mine = 1;
+            minesArr.push({
+                x: mineX,
+                y: mineY
+            });
             num--;
         }
     }
@@ -85,11 +91,13 @@ function init() {
 
 // 点击左键
 function leftClick(dom) {
-    if (dom.classList.contains('isLei')) {
-        var mineblocks = document.getElementsByClassName('isLei');
+    var posX = +dom.id.slice(1).split('-')[0];
+    var posY = +dom.id.slice(1).split('-')[1];
+    if (minesMap[posX][posY].mine) {
         for (var i = 0; i < minesNum; i++) {
-            mineblocks[i].style.backgroundImage = 'url(./img/dilei.jpg)';
-            mineblocks[i].style.backgroundSize = '100% 100%';
+            var mineblock = document.getElementById('p' + minesArr[i].x + '-' + minesArr[i].y);
+            mineblock.style.backgroundImage = 'url(./img/dilei.jpg)';
+            mineblock.style.backgroundSize = '100% 100%';
         }
         setTimeout(function () {
             alertBox.style.display = 'block';
@@ -99,13 +107,10 @@ function leftClick(dom) {
         return;
     } else {
         var num = 0;
-        var pos = {};
-        pos.x = +dom.id.slice(1).split('-')[0];
-        pos.y = +dom.id.slice(1).split('-')[1];
-        for (var i = pos.x - 1; i <= pos.x + 1; i++) {
-            for (var j = pos.y - 1; j <= pos.y + 1; j++) {
+        for (var i = posX - 1; i <= posX + 1; i++) {
+            for (var j = posY - 1; j <= posY + 1; j++) {
                 var nearBox = document.getElementById('p' + i + '-' + j);
-                if (nearBox && nearBox.classList.contains('isLei')) {
+                if (nearBox && minesMap[i][j].mine == 1) {
                     num++;
                 }
             }
@@ -114,8 +119,8 @@ function leftClick(dom) {
         dom.innerHTML = num;
 
         if (num == 0) {
-            for (var i = pos.x - 1; i <= pos.x + 1; i++) {
-                for (var j = pos.y - 1; j <= pos.y + 1; j++) {
+            for (var i = posX - 1; i <= posX + 1; i++) {
+                for (var j = posY - 1; j <= posY + 1; j++) {
                     var nearBox = document.getElementById('p' + i + '-' + j);
                     if (nearBox && !nearBox.classList.contains('check') && !nearBox.classList.contains('flag')) {
                         nearBox.classList.add('check');
@@ -129,17 +134,19 @@ function leftClick(dom) {
 // v2.0: flagNum限制<=10
 // 点击右键
 function rightClick(dom) {
+    var posX = +dom.id.slice(1).split('-')[0];
+    var posY = +dom.id.slice(1).split('-')[1];
     if (dom.classList.contains('num')) {
         return;
     } else {
         if (flagNum > 0) {
             if (dom.classList.toggle('flag')) {
-                if (dom.classList.contains('isLei')) {
+                if (minesMap[posX][posY].mine) {
                     mineover--;
                 }
                 flagNum--;
             } else {
-                if (dom.classList.contains('isLei')) {
+                if (minesMap[posX][posY].mine) {
                     mineover++;
                 }
                 flagNum++;
@@ -148,7 +155,9 @@ function rightClick(dom) {
             if (dom.classList.contains('flag')) {
                 dom.classList.remove('flag');
                 flagNum++;
-                if (dom.classList.contains('isLei')) {
+                if (minesMap[posX][
+                        [posY].mine
+                    ]) {
                     mineover++;
                 }
             }
